aa0b9d3d7dc51fb5d78247bc7eca2cc55a5a4672
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2018-04-17 17:57:28 +0100

    first version works

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,45 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-
   let hoverSlideContainer = document.querySelectorAll('.hover-slide')
-  let getContainers = []
 
   for (let item of hoverSlideContainer) {
     let getChildren = item.querySelectorAll('.hover-slide > *')
-    console.log(getChildren.length);
-    getContainers.push(getChildren);
-    let repeatQuery = '<div class=hover-cover">' + '<div class="hover-segment"></div>'.repeat(getChildren.length) + '</div>'
-    item.insertAdjacentHTML( 'afterbegin', repeatQuery );
+    item.addEventListener('mousemove', function(event) {
+    const x = event.offsetX;
+    const width = this.offsetWidth;
+    const percentage = x / width;
+    // console.log(getChildren.length);
+    const imageNumber = Math.floor(percentage * getChildren.length);
+    console.log(imageNumber);
+    //
+    getChildren.forEach(getChildren => {
+      getChildren.style.zIndex = 0;
+    });
+    //
+    getChildren[imageNumber].style.zIndex = 1;
+
+    })
   }
-
-  document.querySelectorAll('.hover-cover').classList.add('test')
-
-
-
-
-  for (let child of getContainers) {
-    for (let [index, val] of child.entries()) {
-
-      // console.log(val);
-      // console.log(index);
-    }
-  }
-
-
 });
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//
-//   let = hoverSlideContainer = document.querySelectorAll('.hoverslide > *')
-//   let = hoverSlideLength = hoverSlideContainer.length
-//   let array = []
-//
-//   for (var i = 0; i < hoverSlideContainer.length; i++) {
-//     array.push(hoverSlideContainer[i])
-//   }
-//
-//   console.log(array);
-//
-// });
