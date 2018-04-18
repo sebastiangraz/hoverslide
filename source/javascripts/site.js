e0ed51ae2d29d802f0a48c32995c1603ce4e2e79
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2018-04-18 12:06:37 +0100

    more robust structure allowing links inside divs

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,20 +3,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
   for (let item of hoverSlideContainer) {
     let getChildren = item.querySelectorAll('.hover-slide > *')
-    item.addEventListener('mousemove', function(event) {
-    const x = event.offsetX;
-    const width = this.offsetWidth;
-    const percentage = x / width;
-    // console.log(getChildren.length);
-    const imageNumber = Math.floor(percentage * getChildren.length);
-    console.log(imageNumber);
-    //
-    getChildren.forEach(getChildren => {
-      getChildren.style.zIndex = 0;
-    });
-    //
-    getChildren[imageNumber].style.zIndex = 1;
+    let repeatQuery = '<div class="hover-cover">' + '<div class="hover-segment"></div>'.repeat(getChildren.length) + '</div>'
+    item.insertAdjacentHTML( 'afterbegin', repeatQuery );
+    getChildren[0].classList.add('active');
+    console.log(item);
 
+    function relativeCoords ( event ) {
+      var bounds = item.getBoundingClientRect();
+      var x = event.clientX - bounds.left;
+      var y = event.clientY - bounds.top;
+      return {x: x, y: y};
+    }
+    item.addEventListener('mousemove', function() {
+      var rel = relativeCoords(event)
+      let x = rel.x;
+      let width = this.getBoundingClientRect().width;
+      let percentage = x / width;
+      let imageNumber = Math.floor(percentage * getChildren.length);
+
+      getChildren.forEach(getChildren => {
+        getChildren.classList.remove('active')
+      });
+      getChildren[imageNumber].classList.add('active')
     })
+
   }
 });
