489593501c83eac9a9ef7b6a9dde7128ca117557
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2018-04-29 15:18:54 +0100

    first marketing attempt

--- source/javascripts/slideHover.js
+++ source/javascripts/slideHover.js
@@ -157,6 +157,7 @@
 
     var hoverHandler = function( event ) {
       if (!event.target.classList.contains(settings.selector)) return
+      console.log(event.target);
       let getChildren = [].slice.call(event.target.querySelector('.hover-content').children);
       mouseX(event.target, event, getChildren)
     }
@@ -198,56 +199,56 @@
 
 	return HoverSlide;
 });
-//
-//
-//
-// var slideHover = function(selector) {
-//   this.node = document.querySelectorAll(selector);
-//     if (this.node.length == 0) {
-//       console.error('Unable to find the element');
-//     }
-//   return this;
-// }
-//
-// var showSegments = function(selector, childLength) {
-//   let repeatQuery = '<div class="hover-cover">' + '<div class="hover-segment"></div>'.repeat(childLength.length) + '</div>'
-//   selector.insertAdjacentHTML( 'afterbegin', repeatQuery );
-// }
-//
-// // function to get the parents coordinates values
-// var parentCoord = function(selector, event) {
-//   let bounds = selector.getBoundingClientRect();
-//   let x = event.clientX - bounds.left;
-//   let y = event.clientY - bounds.top;
-//   let width = bounds.width;
-//   return {x: x, y: y, width: width};
-// }
-//
-// var setClass = function(children, index) {
-//   for (const child of children) {
-//     child.classList.remove('active')
-//   }
-//   children[index].classList.add('active')
-// }
-//
-// var mouseX = function (selector, event, children) {
-//   let parent = parentCoord(selector, event)
-//   let percentage = parent.x / parent.width;
-//   let imageNumber = Math.floor(percentage * children.length);
-//   setClass(children, imageNumber)
-// }
-//
-// slideHover.prototype.init = function() {
-//   Array.prototype.forEach.call(this.node, function (node) {
-//     // get children of parent selector and convert to array
-//     let getChildren = [].slice.call(node.children);
-//     // add active class to first item in array
-//     getChildren[0].classList.add('active')
-//     // generate overlay to show segments
-//     showSegments(node, getChildren)
-//     // listen for mouse movement and update image
-//     node.addEventListener('mousemove', function(event) {
-//       mouseX(node, event, getChildren)
-//     });
-//   });
-// }
+
+
+
+var slideHover = function(selector) {
+  this.node = document.querySelectorAll(selector);
+    if (this.node.length == 0) {
+      console.error('Unable to find the element');
+    }
+  return this;
+}
+
+var showSegments = function(selector, childLength) {
+  let repeatQuery = '<div class="hover-cover">' + '<div class="hover-segment"></div>'.repeat(childLength.length) + '</div>'
+  selector.insertAdjacentHTML( 'afterbegin', repeatQuery );
+}
+
+// function to get the parents coordinates values
+var parentCoord = function(selector, event) {
+  let bounds = selector.getBoundingClientRect();
+  let x = event.clientX - bounds.left;
+  let y = event.clientY - bounds.top;
+  let width = bounds.width;
+  return {x: x, y: y, width: width};
+}
+
+var setClass = function(children, index) {
+  for (const child of children) {
+    child.classList.remove('active')
+  }
+  children[index].classList.add('active')
+}
+
+var mouseX = function (selector, event, children) {
+  let parent = parentCoord(selector, event)
+  let percentage = parent.x / parent.width;
+  let imageNumber = Math.floor(percentage * children.length);
+  setClass(children, imageNumber)
+}
+
+slideHover.prototype.init = function() {
+  Array.prototype.forEach.call(this.node, function (node) {
+    // get children of parent selector and convert to array
+    let getChildren = [].slice.call(node.children);
+    // add active class to first item in array
+    getChildren[0].classList.add('active')
+    // generate overlay to show segments
+    showSegments(node, getChildren)
+    // listen for mouse movement and update image
+    node.addEventListener('mousemove', function(event) {
+      mouseX(node, event, getChildren)
+    });
+  });
+}
